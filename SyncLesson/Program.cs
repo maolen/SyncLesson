05db1c3634c65943a309a8a8c116a4468a7e92f4
Kamila Zhakupova <zhakupova.kamila@gmail.com>
2019-12-09 19:41:43 +0600

    Add project files.

---
+++ SyncLesson/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SyncLesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
